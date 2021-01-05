e41f0c1f9506c858f508bf3b6f04fa116e7d144a
clement-boret <clement.boret@tidal.com>
2021-01-05 12:03:44 +0100

    mvn clean install works but vscode shows errors

---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+        new javax.xml.namespace.QName("http://www.w3.org/2001/XMLSchema", "string");
+	}
+
+}
