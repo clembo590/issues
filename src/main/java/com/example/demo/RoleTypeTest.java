57c51d705a7617ecaf7f7b463a5bfde36884e597
clem <clembo590@gmail.com>
2022-06-08 21:07:04 +0200

    mvn clean install works

---
+++ src/main/java/com/example/demo/RoleTypeTest.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RoleTypeTest {
+
+    @Test
+    public void testDeanery() {
+        assertEquals(Boolean.TRUE, Boolean.TRUE);
+    }
+}
